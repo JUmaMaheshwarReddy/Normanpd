c5856f618a932557d1b31471010d9350ba30de1a
JUmaMaheshwarReddy <umamaheshwar00@.gmail.com>
2022-03-03 05:26:45 +0000

    ssl

--- project0/project0.py
+++ project0/project0.py
@@ -4,3 +4,22 @@ import ssl
 import tempfile
 import PyPDF2
 import sqlite3
+
+
+try:
+    ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def fetchincidents(url):
+
+    #url = ("https://www.normanok.gov/sites/default/files/documents/2022-01/2022-01-01_daily_incident_summary.pdf")
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"
+    data = urllib.request.urlopen(
+        urllib.request.Request(
+            url, headers=headers)).read()
+    return data
