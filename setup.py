a154089c4a80e882f3d96166b871d12a016330c5
JUmaMaheshwarReddy <umamaheshwar00@.gmail.com>
2022-03-09 02:18:33 +0000

    final

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='project0',
     version='1.0',
-    author='Uma Maheshwar Reddy',
+    author='J Uma Maheshwar Reddy',
     author_email='umamaheshwarreddy.jangalapalli@ou.edu',
     packages=find_packages(exclude=('tests', 'docs')),
     setup_requires=['pytest-runner'],
